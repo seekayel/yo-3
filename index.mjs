2fa98ac1ea417dd025f763ad2d569647be80ad95
Mike <korostelevm@gmail.com>
2022-12-16 13:28:46 -0500

    mjs

---
+++ index.mjs
@@ -0,0 +1,41 @@
+// const express = require('express')
+import express from 'express';
+import cors from 'cors';
+const app = express()
+const router = express.Router()
+
+
+app.use(cors())
+
+
+app.use('/',router)
+router.post('/', (req, res)=>{
+   
+res.send('yo')
+})
+
+router.get('/process_exit', (req, res)=>{
+     process.exit()
+
+})
+
+router.get('*', (req, res)=>{
+   console.log(req.url)
+    console.log('asdfasdfasf')
+    console.log(Date.now())
+    console.log(req.query)
+       console.log(req.headers)
+   
+      res.setHeader('WWW-Authenticate','Basic')
+      res.setHeader('Access-Control-Allow-Origin','*')
+      res.setHeader('Access-Control-Allow-Methods','*')
+      res.setHeader('Access-Control-Allow-Headers','*')
+      res.setHeader('Access-Control-Allow-Credentials',true)
+   
+    return res.json({
+       yo:'yo',
+       url: req.url
+    })
+})
+
+app.listen(3000)
