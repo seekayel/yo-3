1ce0fbccc13ab70db86b13e3192c02d3bac3cff0
Mike Korostelev <korostelevm@gmail.com>
2022-12-16 11:56:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,14 @@ router.get('*', (req, res)=>{
     console.log('asdfasdfasf')
     console.log(Date.now())
     console.log(req.query)
-    console.log(req.headers)
+       console.log(req.headers)
+   
+      res.setHeader('WWW-Authenticate','Basic')
+      res.setHeader('Access-Control-Allow-Origin','*')
+      res.setHeader('Access-Control-Allow-Methods','*')
+      res.setHeader('Access-Control-Allow-Headers','*')
+      res.setHeader('Access-Control-Allow-Credentials',true)
+   
     return res.json({
        yo:'yo',
        url: req.url
