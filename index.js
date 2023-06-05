c1a5f25b5968a2993437b8421da05eb2744ef0f1
Kam <ckl@seekayel.com>
2023-06-04 21:06:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const router = express.Router()
@@ -8,6 +9,24 @@ const router = express.Router()
 // app.use(cors())
 
 
+// Define the function to synchronously list files in a directory
+const listFilesInDir = (dir) => {
+    let files = [];
+    try {
+        files = fs.readdirSync(dir);
+    } catch (err) {
+        console.error(`Error reading directory: ${dir}`);
+        console.error(err);
+        return;
+    }
+
+    console.log(`############################### Files in ${dir}:`);
+    files.forEach((file) => {
+        console.log(file);
+    });
+    console.log('\n');
+}
+
 app.use('/',router)
 router.post('/', (req, res)=>{
    
@@ -16,17 +35,21 @@ res.send('yo')
 
 
 router.get('/', (req, res)=>{
-   console.log(req.url)
-    console.log('asdfasdfasf')
-    console.log(Date.now())
-    console.log(req.query)
-       console.log(req.headers)
+   const dirs = process.env.PATH.split(':').reverse();
+   // Call the function for each directory
+   dirs.forEach(listFilesInDir);
    
-      res.setHeader('WWW-Authenticate','Basic')
-      res.setHeader('Access-Control-Allow-Origin','*')
-      res.setHeader('Access-Control-Allow-Methods','*')
-      res.setHeader('Access-Control-Allow-Headers','*')
-      res.setHeader('Access-Control-Allow-Credentials',true)
+   console.log(req.url)
+   console.log('asdfasdfasf')
+   console.log(Date.now())
+   console.log(req.query)
+   console.log(req.headers)
+
+   res.setHeader('WWW-Authenticate','Basic')
+   res.setHeader('Access-Control-Allow-Origin','*')
+   res.setHeader('Access-Control-Allow-Methods','*')
+   res.setHeader('Access-Control-Allow-Headers','*')
+   res.setHeader('Access-Control-Allow-Credentials',true)
    
     return res.json({
        yo:'yo1',
